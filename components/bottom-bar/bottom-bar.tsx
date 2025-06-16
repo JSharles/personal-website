db14d75e4ff8e5dbb91421fc54f44e2d4d9e8ab2
JSharles <jeancharlesbarq@gmail.com>
2025-06-16 21:05:40 +0200

    fix: mobile breakpoint

--- components/bottom-bar/bottom-bar.tsx
+++ components/bottom-bar/bottom-bar.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { useIsMobile } from "@/hooks/use-mobile";
 import { SocialLinks } from "../social-links/social-links";
 
 export const BottomBar = () => {
+  const isMobile = useIsMobile();
+
+  if (!isMobile) return null;
+
   return (
-    <div className="fixed w-screen bottom-4 px-4 flex justify-center z-50 md:hidden pointer-events-none">
+    <div className="fixed w-screen bottom-4 px-4 flex justify-center z-50 pointer-events-none">
       <div className="pointer-events-auto">
         <SocialLinks />
       </div>
